cf3bd0a41964440a2a01af5af766f63494a07768
David Rodríguez <davrodrila@gmail.com>
2018-12-08 18:34:28 +0100

    Fixed bad redirection while deleting the subscriber

--- resources/js/components/DeleteSubscriber.vue
+++ resources/js/components/DeleteSubscriber.vue
@@ -3,14 +3,12 @@
 </template>
 
 <script>
-
-
     export default {
         props: ['id'],
         methods: {
             read(id) {
                 window.axios.delete('/api/v1/subscribers/' + id).then(({data}) => {
-                    this.$router.push('/show/' + this.$route.params.id);
+                    this.$router.push('/');
                 });
             }
         },
